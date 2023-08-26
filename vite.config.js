7eb0052135262aa1e9c2c44a1be88975599bc0a1
yuchih <68271254+yuchih-hu@users.noreply.github.com>
2023-08-26 22:51:38 +0800

    Deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: (process.env.NODE_ENV === 'production') ? 'react-workshop-hw3/' : '/',
   plugins: [react()],
 })
